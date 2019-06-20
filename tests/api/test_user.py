d46a2818f9f8579928702488f061e696f31b7985
Hans Jerry Illikainen <hji@dyntopia.com>
2019-06-20 20:05:59 +0000

    api: tests: don't use hardcoded status codes

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from pytest import mark
+from rest_framework import status
 
 
 @mark.django_db
 def test_unauthenticated(client):
     user = User.objects.create_user(username='foo', password='bar')
-    assert client.get('/api/user/').status_code == 403
-    assert client.get('/api/user/{}/'.format(user.id)).status_code == 403
+    assert client.get('/api/user/').status_code == status.HTTP_403_FORBIDDEN
+    assert client.get('/api/user/{}/'.format(user.id)).status_code == \
+        status.HTTP_403_FORBIDDEN
 
 
 @mark.django_db
@@ -16,11 +18,11 @@ def test_list(client):
 
     client.login(username='foo', password='bar')
     res = client.get('/api/user/')
-    assert res.status_code == 403
+    assert res.status_code == status.HTTP_403_FORBIDDEN
 
     client.login(username='x', password='y')
     res = client.get('/api/user/')
-    assert res.status_code == 200
+    assert res.status_code == status.HTTP_200_OK
     assert len(res.data) == 2
     assert res.data[0]['id'] == user.id
     assert res.data[1]['id'] == staff.id
@@ -32,7 +34,7 @@ def test_retrieve(client):
 
     client.login(username='foo', password='bar')
     res = client.get('/api/user/{}/'.format(user.id))
-    assert res.status_code == 200
+    assert res.status_code == status.HTTP_200_OK
     assert res.data['id'] == user.id
     assert res.data['username'] == user.username
 
@@ -45,10 +47,10 @@ def test_retrieve_other(client):
 
     client.login(username='foo', password='bar')
     res = client.get('/api/user/{}/'.format(user.id))
-    assert res.status_code == 403
+    assert res.status_code == status.HTTP_403_FORBIDDEN
 
     client.login(username='x', password='y')
     res = client.get('/api/user/{}/'.format(user.id))
-    assert res.status_code == 200
+    assert res.status_code == status.HTTP_200_OK
     assert res.data['id'] == user.id
     assert res.data['username'] == user.username
